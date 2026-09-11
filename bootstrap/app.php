a277cdddee4f77f59ec549c78bd3adc179ed2cd6
Jaikhlang Brahma <jaikhlang@gmail.com>
2026-09-11 10:54:44 +0530

    infrastructure-setup-update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,14 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                    Request::HEADER_X_FORWARDED_HOST |
+                    Request::HEADER_X_FORWARDED_PORT |
+                    Request::HEADER_X_FORWARDED_PROTO |
+                    Request::HEADER_X_FORWARDED_AWS_ELB
+        );
         $middleware->alias([
             'role' => EnsureUserHasRole::class,
         ]);
